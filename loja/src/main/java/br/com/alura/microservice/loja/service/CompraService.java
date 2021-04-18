a655fc270c351d96193409ae479d2372e14aa6d6
Elias <silva.elias@totvs.com.br>
2021-04-18 15:18:20 -0300

    add config server and fornecedor.yml on root

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,9 +11,20 @@ import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDto;
 @Service
 public class CompraService {
 
+	// pega o restTemplate, provido no LojaApplication
+	// esse rest template, verifica no eureka os servicos, e
+	// devolve o fornecedor-service para nós
+	@Autowired
+	private RestTemplate client;
+
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate client = new RestTemplate();
-		String url = "http://fornecedor/info/" + compra.getEndereco().getEstado();
+//		RestTemplate client = new RestTemplate();
+//		String url = "http://fornecedor/info/" + compra.getEndereco().getEstado();
+//		var response = client.exchange(url, HttpMethod.GET, null, InfoFornecedorDto.class);
+//
+//		System.out.println(response.getBody().getEndereco());
+
+		String url = "http://fornecedor-service/info/" + compra.getEndereco().getEstado();
 		var response = client.exchange(url, HttpMethod.GET, null, InfoFornecedorDto.class);
 
 		System.out.println(response.getBody().getEndereco());
